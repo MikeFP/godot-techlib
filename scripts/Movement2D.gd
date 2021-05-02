a0275b50c63c338dac0c8beb8c71b71b82664eb3
MikeFP <mikefp28@gmail.com>
2021-05-02 12:41:22 -0400

    Simple animated 2D character

--- scripts/Movement2D.gd
+++ scripts/Movement2D.gd
@@ -1,6 +1,13 @@
 extends KinematicBody2D
 
 export var speed := 300.0
+export var sprite_path : NodePath
+export var anim_tree_path: NodePath
+
+onready var sprite = get_node(sprite_path)
+onready var anim_tree = get_node(anim_tree_path)
+
+var gaze = Vector2.RIGHT
 
 func _physics_process(_delta):
 	var dir = Vector2.ZERO
@@ -8,6 +15,23 @@ func _physics_process(_delta):
 		dir += Vector2.RIGHT
 	elif Input.is_action_pressed("move_left"):
 		dir += Vector2.LEFT
+
+	if dir.x > 0:
+		gaze = Vector2.RIGHT
+	elif dir.x < 0:
+		gaze = Vector2.LEFT
 	
-	if dir != Vector2():
+	if gaze == Vector2.RIGHT:
+		sprite.scale = Vector2(1, 1)
+	else:
+		sprite.scale = Vector2(-1, 1)
+	
+	var zero_move = dir == Vector2()
+	
+	if !zero_move:
 		move_and_slide(dir * speed)
+
+	if anim_tree != null:
+		anim_tree["parameters/conditions/walking"] = !zero_move
+		anim_tree["parameters/conditions/idle"] = zero_move
+		
